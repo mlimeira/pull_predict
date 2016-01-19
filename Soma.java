0b837456c3adbe1fcbd6d62fb34aaded8ebea737
Manoel Limeira <juniorlimeiras@gmail.com>
2016-01-19 15:59:05 -0500

    Create Soma.java

---
+++ Soma.java
@@ -0,0 +1,10 @@
+public class Soma{
+  public static void main (String [] args){
+    int a=0, b=0, soma=0;
+    Scanner ler = new Scanner(System.in);
+    a = ler.nextInt();
+    b = ler.nextInt();
+    soma = a +b;
+    System.out.println("Soma:"+soma);
+  }
+}
