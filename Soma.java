fea0233fb98cee98ff49271c8b6bb8dc68eec9fa
Manoel Limeira de Lima Júnior <mlimeira@ic.uff.br>
2019-04-11 10:44:32 -0500

    Update Soma.java
    Sum double.

--- Soma.java
+++ Soma.java
@@ -1,9 +1,9 @@
 public class Soma{
   public static void main (String [] args){
-    int a=0, b=0, soma=0;
+    double a=0, b=0, soma=0;
     Scanner ler = new Scanner(System.in);
-    a = ler.nextInt();
-    b = ler.nextInt();
+    a = ler.nextDouble();
+    b = ler.nextDouble();
     soma = a +b;
     System.out.println("Soma:"+soma);
   }
